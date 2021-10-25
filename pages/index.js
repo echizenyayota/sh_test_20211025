5c6442cefbb4597fcf647b2e002e08d3bd0eddfc
echizenyayota <virtualmisso30@gmail.com>
2021-10-25 15:49:47 +0900

    Add the area of the data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import { Button, Card, Heading, Page, Stack, TextField } from "@shopify/polaris";
+import { Button, Card, Datatable, EmptyState, Heading, Page, Stack, TextField } from "@shopify/polaris";
 import { ResourcePicker } from "@shopify/app-bridge-react";
 
 const Index = () => {
@@ -10,6 +10,8 @@ const Index = () => {
   const [pickerOpen, setPickerOpen] = useState(false);
   const [products, setProducts] = useState([]);
   const [showToast, setShowToast] = useState(false);
+
+  const productTableDisplayData = products.map((product) => product.id);
 
   return (
     <Page>
@@ -44,6 +46,13 @@ const Index = () => {
             />
             <Button primary onClick={() => setPickerOpen(true)}>Select Products</Button>
           </Stack>
+        </Card.Section>
+        <Card.Section>
+          {productTableDisplayData.length ? <DataTable
+            columnContentTypes={[]}
+            headings={[]}
+            rows={[]}
+          /> : <EmptyState heading="no products selection"/>}
         </Card.Section>
       </Card>
     </Page>
